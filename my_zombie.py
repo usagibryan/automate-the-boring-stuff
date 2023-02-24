0d80d563c7e19f6090a4d06d0eb84842d29cf147
usagibryan <34449029+usagibryan@users.noreply.github.com>
2023-02-24 09:31:17 -0500

    Sync

--- my_zombie.py
+++ my_zombie.py
@@ -255,5 +255,5 @@ zombies = (
 )
 
 # Uncomment one of the following lines to run in CLI or Web GUI mode:
-#zombiedice.runTournament(zombies=zombies, numGames=1000)
+# zombiedice.runTournament(zombies=zombies, numGames=1000)
 zombiedice.runWebGui(zombies=zombies, numGames=1000)
